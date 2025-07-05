e223d56e45af3339993eb6e9a43742864e2783c2
juanandrescl <92060327+juanandrescl@users.noreply.github.com>
2025-07-05 20:26:48 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,78 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "animacion 1":
+            case "yb/Bn{?P{eSY$rdmicnu":return [img`
+3 3 . . . . . . . . . . . . . . 
+3 3 3 . . . . . . . . . . . . . 
+. 3 3 3 . . . . . . . . . . . . 
+. . . 3 3 . . . . . . . . . . . 
+. . . . 3 3 . . . . . . . . . . 
+. . . . . 3 . . . . . . . . . . 
+. . . . . . . . 1 . . . . . . . 
+. . . . . . . . . . . . 3 3 . . 
+. . . . . . . . . . . 1 . 3 . . 
+. . . . . . . . . 1 1 3 3 3 . . 
+. . . . . . . 3 3 3 3 3 . . . . 
+. 3 3 3 3 3 3 3 3 3 3 . . . . . 
+. 3 3 . 3 3 3 . 3 3 3 3 . . . . 
+. . . . . . 3 . . . 3 . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f 3 3 3 f f f f f f 
+f f f f f 3 3 f f f 3 f f f f f 
+f f f f f 3 3 f f f 3 f f f f f 
+f f f f 3 f 3 f f f 3 f f f f f 
+f f f f 3 f 3 f f f 3 f f 3 f f 
+f f f f 3 f 3 f f f 3 f f 3 f f 
+f f f f 3 f 3 f f f 3 3 f 3 f f 
+f f f f f 3 3 f f f f 3 f 3 3 f 
+f f f f f f f f f f f 3 3 f 3 f 
+f f f f f f f f f f f f 3 f f f 
+f f f f f f f f f f f f 3 f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . 3 3 3 3 3 3 
+. . . . . . . . . 3 3 . . . . 3 
+. . . . . . . 3 3 3 3 3 . . . 3 
+. . . . . . 3 . . . 3 3 . . . . 
+. . . . . . 3 . . . . 3 . . . . 
+. . . . . 3 . 3 3 3 . . . . . . 
+. . . . . 3 . . . 3 . . . . . . 
+. . . . . 3 . . . 3 . . . f . . 
+. . . . . . 3 3 3 . . . . f f f 
+. . . . . . . . . . . . . f f f 
+. . . . . . . . . . . . . f f . 
+. . . . . . . . . . . . f . . . 
+. . . . . . . . . . . . f f f . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
